59493d4c1e7628640ecc622230f23584aeb5604b
Jörg Pfeifer <jeypack@users.noreply.github.com>
2024-07-25 19:53:40 +0200

    Completed Chapter 10

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,13 @@
 
 const nextConfig = {};
 
+//The 'incremental' value allows you to adopt PPR for specific routes.
+//Next, add the experimental_ppr segment config option to your dashboard layout:
+///app/dashboard/layout.tsx : export const experimental_ppr = true;
+/* const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
+}; */
+
 export default nextConfig;
